3d340259abb71c4949a8a8e813e7fdeaf8fef64f
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-11-28 14:07:58 +0100

    Added CardSection component, finished Section7

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Card from './Card'; //because I want to wrap AlbumList within Card component
+import CardSection from './CardSection';
 
 const AlbumList = (props) => {
 
     return (
     <Card>
-        <Text>{props.album.title}</Text>
+        <CardSection>
+            <Text>{props.album.title}</Text>
+        </CardSection>
     </Card>
     );
 };
